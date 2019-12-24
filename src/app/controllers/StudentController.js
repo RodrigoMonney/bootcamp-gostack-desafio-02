642293a26cdf9a4666ebf84d4cd180582d538e9c
Rodrigo Reis <rodrigo.monney@gmail.com>
2019-12-24 05:34:10 -0300

    chore: add input validation

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .integer()
+        .moreThan(1),
+      weight: Yup.number().moreThan(0),
+      height: Yup.number().moreThan(0),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
     const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
